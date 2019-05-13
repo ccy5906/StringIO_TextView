125d26b75f441eb00df6a027a7d037472f275c11
Jiyeon Choi <ccy5906@naver.com>
2019-05-13 09:48:43 +0900

    #1 문자 입출력 폼 디자인 - 텍스트박스, 버튼, 라벨 추가

--- StringIO_TextView/Form1.Designer.cs
+++ StringIO_TextView/Form1.Designer.cs
@@ -29,12 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lblReault = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.txtEdit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Tahoma", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(12, 39);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(307, 71);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "입력";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // lblReault
+            // 
+            this.lblReault.AutoSize = true;
+            this.lblReault.Font = new System.Drawing.Font("안상수2006가는", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblReault.Location = new System.Drawing.Point(12, 113);
+            this.lblReault.Name = "lblReault";
+            this.lblReault.Size = new System.Drawing.Size(54, 21);
+            this.lblReault.TabIndex = 2;
+            this.lblReault.Text = "결과 : ";
+            // 
+            // txtEdit
+            // 
+            this.txtEdit.Location = new System.Drawing.Point(12, 12);
+            this.txtEdit.Name = "txtEdit";
+            this.txtEdit.Size = new System.Drawing.Size(307, 21);
+            this.txtEdit.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(331, 314);
+            this.Controls.Add(this.txtEdit);
+            this.Controls.Add(this.lblReault);
+            this.Controls.Add(this.btnEdit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "문자입출력";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label lblReault;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtEdit;
     }
 }
 
