7f910dacc78a9f03e88133a0e7dc7115ff786a75
Jiyeon Choi <ccy5906@naver.com>
2019-05-13 10:10:56 +0900

    #2 사용자 입력을 라벨에 출력

--- StringIO_TextView/Form1.cs
+++ StringIO_TextView/Form1.cs
@@ -11,9 +11,20 @@ namespace StringIO_TextView
 {
     public partial class Form1 : Form
     {
+        private string OrgStr="";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblReault.Text;
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.lblReault.Text = this.OrgStr + this.txtEdit.Text;
         }
     }
 }
